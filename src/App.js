b38a38443af1f8e05cc3c7bc480cb61d08cbd2cd
Hilmi-z <hilmiouelhazi@example.com>
2023-08-29 07:48:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+import TaskForm from "./TaskForm";
+import Task from "./Task";
+import {useEffect, useState} from "react";
+
+function App() {
+  const [tasks,setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasks || []);
+  }, []);
+
+  function addTask(name) {
+    setTasks(prev => {
+      return [...prev, {name:name,done:false}];
+    });
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks(prev => {
+      return prev.filter((taskObject,index) => index !== indexToRemove);
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const numberComplete = tasks.filter(t => t.done).length;
+  const numberTotal = tasks.length;
+
+  function getMessage() {
+    const percentage = numberComplete/numberTotal * 100;
+    if (percentage === 0) {
+      return 'Try to do at least one! ';
+    }
+    if (percentage === 100) {
+      return 'Nice job for today! ';
+    }
+    return 'Keep going !';
+  }
+
+  function renameTask(index,newName) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    })
+  }
+
+  return (
+    <main>
+      <h1><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>TO-DO LIST</h1>
+      <h2>{numberComplete}/{numberTotal} Complete</h2>
+      <h3>{getMessage()}</h3>
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task,index) => (
+        <Task {...task}
+              onRename={newName => renameTask(index,newName)}
+              onTrash={() => removeTask(index)}
+              onToggle={done => updateTaskDone(index, done)} />
+      ))}
+      
+
+
+
+    </main>
+  );
+}
+
+export default App;
---
+++ src/TaskForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+export default function TaskForm({ onAdd }) {
+    const [taskName, setTaskName] = useState('');
+
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        if (taskName.trim() === '') {
+            return; // Prevent adding empty tasks
+        }
+        onAdd(taskName);
+        setTaskName('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <button type="submit">+</button>
+            <input
+                type="text"
+                value={taskName}
+                onChange={(ev) => setTaskName(ev.target.value)}
+                placeholder="Your next task..."
+            />
+        </form>
+    );
+}
